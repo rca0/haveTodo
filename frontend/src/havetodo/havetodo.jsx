da6bd1a84d8892d7300b723f817008703dbadbb4
Ruan Arcega <ruanarcega@gmail.com>
2019-11-11 01:30:30 -0300

    feat: add event

--- frontend/src/havetodo/havetodo.jsx
+++ frontend/src/havetodo/havetodo.jsx
@@ -5,11 +5,20 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class haveTodo extends Component {
+    constructor(props) {
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd() {
+        console.log(this)
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Tasks' small='Register' />
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
