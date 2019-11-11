486e9b3f99bacecf5cb06c6a1953c97243dade23
Ruan Arcega <ruanarcega@gmail.com>
2019-11-11 00:43:51 -0300

    feat: configure menu

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,9 +4,11 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
 import HaveTodo from '../havetodo/havetodo'
 import About from '../about/about'
+import Menu from '../template/menu'
 
 export default props => (
     <div className='container'>
+        <Menu />
         <HaveTodo />
         <About />
     </div>
