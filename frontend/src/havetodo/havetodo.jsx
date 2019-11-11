e1881a60ff5d33e4123c34b7fbd1b61addfc9a59
Ruan Arcega <ruanarcega@gmail.com>
2019-11-11 00:58:07 -0300

    feat: add todolist and todoform components

--- frontend/src/havetodo/havetodo.jsx
+++ frontend/src/havetodo/havetodo.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 
 import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
 
 export default class haveTodo extends Component {
     render() {
         return (
             <div>
                 <PageHeader name='Tasks' small='Register' />
+                <TodoForm />
+                <TodoList />
             </div>
         )
     }
