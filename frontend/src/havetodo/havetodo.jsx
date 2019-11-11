3505d7dc1d9ac69a0be1037fe5dff6ffb45d3c4f
Ruan Arcega <ruanarcega@gmail.com>
2019-11-11 00:35:48 -0300

    feat: configure component about

---
+++ frontend/src/havetodo/havetodo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class haveTodo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>haveTodo</h1>
+            </div>
+        )
+    }
+}
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,9 +2,12 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import HaveTodo from '../havetodo/havetodo'
+import About from '../about/about'
 
 export default props => (
     <div className='container'>
-        <h1>Hello world</h1>
+        <HaveTodo />
+        <About />
     </div>
 )
