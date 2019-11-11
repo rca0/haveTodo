d88245b6a720a18811d7b901c3d98983bc2fa9d9
Ruan Arcega <ruanarcega@gmail.com>
2019-11-11 01:39:59 -0300

    feat: onchange event

--- frontend/src/havetodo/havetodo.jsx
+++ frontend/src/havetodo/havetodo.jsx
@@ -7,7 +7,17 @@ import TodoList from './todoList'
 export default class haveTodo extends Component {
     constructor(props) {
         super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleChange(evt) {
+        this.setState({
+            ...this.state,
+            description: evt.target.value
+        })
     }
 
     handleAdd() {
@@ -18,7 +28,10 @@ export default class haveTodo extends Component {
         return (
             <div>
                 <PageHeader name='Tasks' small='Register' />
-                <TodoForm handleAdd={this.handleAdd} />
+                <TodoForm
+                    description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
--- frontend/src/havetodo/todoForm.jsx
+++ frontend/src/havetodo/todoForm.jsx
@@ -7,8 +7,12 @@ export default props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
             <input id='description' className='form-control'
-                placeholder='Add task'></input>
+                placeholder='Add task'
+                onChange={props.handleChange}
+                value={props.description}>
+                </input>
         </Grid>
+
         <Grid cols='12 3 2'>
             <IconButton style='primary' icon='plus'
                 onClick={props.handleAdd}></IconButton>
