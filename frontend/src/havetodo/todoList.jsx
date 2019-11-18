419dfa7300ecbd7d99f84bae10ce99f97f1b6f43
Ruan Arcega <ruanarcega@gmail.com>
2019-11-18 01:53:05 -0300

    feat: mark as done and pending with redux

--- frontend/src/havetodo/todoList.jsx
+++ frontend/src/havetodo/todoList.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import IconButton from '../template/iconButton'
+import { bindActionCreators } from 'redux'
+import { markAsDone, markAsPending } from './havetodoActions'
 
 const TodoList = props => {
 
@@ -12,10 +14,10 @@ const TodoList = props => {
                 <td className={todo.done ? 'markAsDone' : ''}>{todo.description}</td>
                 <td>
                     <IconButton style='success' icon='check' hide={todo.done}
-                        onClick={() => props.handleMarkAsDone(todo)}>
+                        onClick={() => props.markAsDone(todo)}>
                     </IconButton>
                     <IconButton style='warning' icon='undo' hide={!todo.done}
-                        onClick={() => props.handleMarkAsPending(todo)}>
+                        onClick={() => props.markAsPending(todo)}>
                     </IconButton>
                     <IconButton style='danger' icon='trash-o' hide={!todo.done}
                       onClick={() => props.handleRemove(todo)}>
@@ -41,5 +43,8 @@ const TodoList = props => {
 }
 
 const mapStateToProps = state => ({ list: state.todo.list })
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+    markAsDone, markAsPending
+}, dispatch)
 
-export default connect(mapStateToProps)(TodoList)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
