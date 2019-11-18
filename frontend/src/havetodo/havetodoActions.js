997c707085df4708c0cf90545f7af646511174cb
Ruan Arcega <ruanarcega@gmail.com>
2019-11-18 02:21:18 -0300

    fix: field id name

--- frontend/src/havetodo/havetodoActions.js
+++ frontend/src/havetodo/havetodoActions.js
@@ -11,10 +11,8 @@ export const search = () => {
     return (dispatch, getState) => {
         const description = getState().todo.description
         const search = description ? `&description__regex=/${description}/` : ''
-        const req = axios.get(`${URL}?sort=-createdAt${search}`)
-            .then(resp => dispatch(
-                { type: 'TODO_SEARCHED', payload: resp.data }
-            ))
+        const request = axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(resp => dispatch({type: 'TODO_SEARCHED', payload: resp.data}))
     }
 }
 
@@ -28,21 +26,21 @@ export const add = (description) => {
 
 export const markAsDone = (todo) => {
     return dispatch => {
-        axios.put(`${URL}/${todo.__id}`, { ...todo, done: true })
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
             .then(resp => dispatch(search()))
     }
 }
 
 export const markAsPending = (todo) => {
     return dispatch => {
-        axios.put(`${URL}/${todo.__id}`, { ...todo, done: false })
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
             .then(resp => dispatch(search()))
     }
 }
 
 export const remove = (todo) => {
     return dispatch => {
-        axios.delete(`${URL}/${todo.__id}`)
+        axios.delete(`${URL}/${todo._id}`)
             .then(resp => dispatch(search()))
     }
 }
